0b1f55b2479a112d3094912996c348f664782645
Pallavi Das <palldas37@gmail.com>
2024-09-02 19:31:13 -0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -65,7 +65,7 @@ const Home: React.FC = () => {
         h={{ base: '80%', md: '50%', lg: '30%' }}
         flexShrink={0}
       >
-        <Avatar src="static/me1.jpg" size="full" />
+        <Avatar src="static/me1.JPG" size="full" />
       </Box>
     </Flex>
   );
